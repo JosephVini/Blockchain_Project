410f50346f5b35ef994d2b26ceb6652368550e31
José Vinicius <sjosevinicius254@gmail.com>
2024-08-26 20:29:42 -0300

    Adicionado o WebSocket para aceitar a conexão P2P

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const Blockchain = require('../blockchain');
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
+const P2pServer = require('./p2p-server');
 
 const app = express();
 const bc = new Blockchain();
+const p2pServer = new P2pServer(bc);
 
 app.get('/blocks', (req, res) => {
     res.json(bc.chain);
@@ -17,8 +19,8 @@ app.post('/mine', (req, res) => {
     res.redirect('/blocks');
 });
 
-
 app.listen(HTTP_PORT, () => {
     console.log(`Listening on port ${HTTP_PORT}`);
 });
 
+p2pServer.listen();
